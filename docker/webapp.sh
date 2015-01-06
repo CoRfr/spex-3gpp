8b1b05c973be60f54ef2ced82cd245d2b5755b56
Bertrand Roussel <broussel@sierrawireless.com>
2015-01-06 08:41:29 +0100

    Missing assets:precompile

--- docker/webapp.sh
+++ docker/webapp.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake db:migrate)"
+su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake db:migrate )"
+su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake assets:precompile )"
+
