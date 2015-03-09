13275071350db6baa79ba41972d0784b757df2b6
Bertrand Roussel <bertrand.roussel@cor-net.org>
2015-03-08 17:52:04 -0700

    Update

---
+++ docker/dev.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if docker ps | grep spex-app; then
+  docker kill spex-app
+  docker rm spex-app
+fi
+
+if ! docker ps | grep mysql; then
+  docker run \
+    --name spex-mysql \
+    -e DB_USER='spex'\
+    -e DB_PASS='' \
+    -e DB_NAME='spex' \
+    -d \
+    sameersbn/mysql:latest
+fi
+
+TI=''
+if [ -n "$1" ]; then
+  TI="-ti"
+fi
+
+if docker ps -a | grep spex-app; then
+  docker rm spex-app
+fi
+
+docker run \
+  --name spex-app \
+  --rm \
+  -v /storage/web/specs:/home/app/webapp/specs \
+  -v $DIR/database.yml:/home/app/webapp/config/database.yml \
+  -v $DIR/webapp.sh:/etc/my_init.d/webapp.sh \
+  --link spex-mysql:mysql \
+  -p 127.0.0.1:3000:80 \
+  $TI \
+  corfr/spex $*
+
--- docker/webapp.sh
+++ docker/webapp.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+env
 su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake db:migrate )"
 su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake assets:precompile )"
 
