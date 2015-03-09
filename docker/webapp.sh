efd7957f9873536a8f1aab4fe3d08d3029f4bf77
Bertrand Roussel <bertrand.roussel@cor-net.org>
2015-03-09 03:15:17 +0100

    Update docker startup

--- docker/webapp.sh
+++ docker/webapp.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-env
-su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake db:migrate )"
-su -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake assets:precompile )"
+# Provide db env vars to passenger
+for var in $(env |grep MYSQL | tr '=' ' ' |awk '{print $1}'); do
+	if [ -n "${!var}" ]; then
+		sed -i '$i'"passenger_set_cgi_param $var ${!var};" /etc/nginx/sites-enabled/webapp.conf
+	fi
+done
 
+su --preserve-environment -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake db:migrate )"
+su --preserve-environment -c /bin/bash - app -c "( export RAILS_ENV=production && cd /home/app/webapp && bundle exec rake assets:precompile )"
